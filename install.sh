ca2ae0d58e90ff18efd3a7650ed75ab4c2554582
anonymous <not-set@example.org>
2026-01-29 10:00:05 -0500

    install script

--- install.sh
+++ install.sh
@@ -5,5 +5,14 @@ cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 cp gdbinit ~/.gdbinit
 cp gitconfig ~/.gitconfig
-echo "please install tmux"
-echo "please install oh-my-zsh"
+if ! command -v tmux &> /dev/null; then
+    echo "❌ not found: tmux is not installed. please install"
+else
+    echo "You've got tmux, great!"
+fi
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "📂 Oh My Zsh directory found. great!"
+else
+  echo "❓ Oh My Zsh directory not found."
+fi
